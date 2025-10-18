99ec1843b652c091da0e4592db5243f28b6df524
jayangabandara <jayby006@mymail.unisa.edu.au>
2025-10-18 15:12:11 +1030

    Hacker class and constructor created with starter CryptoToken

--- Hacker.py
+++ Hacker.py
@@ -1,8 +1,18 @@
 """
 File: Hacker.py
 Description: <A brief description of this Python module.>
-Author: <full name>
-ID: <student_id>
-Username: <username>
+Author: Jayanga Madushanka Bandara Bathabure Gedara
+ID: 110432974
+Username: jayby006
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+from asset import Asset
+
+Trace_limit = 5
+
+class Hacker:
+    def __init__(self,name):
+        self.__name = name
+        self.__inventory = [Asset("CryptoTocken", "Starter token")]
+        self.__rig = None
+        self.__trace = 0
